7a556fb5d2e0932ec550d5bc159b4109f0cc1f69
USER-1 <USER-1@USER-1>
2023-10-26 20:10:10 +0500

    MIGRATE: Add Migration

--- Apteka.Backend/Model/Admin.cs
+++ Apteka.Backend/Model/Admin.cs
@@ -1,11 +1,22 @@
-﻿namespace Apteka.Backend.Model
+﻿using LinqToDB.Mapping;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Apteka.Backend.Model
 {
     public class Admin
     {
+        [Identity]
         public Guid Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Login { get; set; }
+        [PasswordPropertyText]
+        [MaxLength(8)]
         public string Password { get; set; }
 
     }
