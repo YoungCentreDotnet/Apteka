639e3787ade189721ff496dddb8ada297ff31b6b
USER-1 <USER-1@USER-1>
2023-10-26 20:42:15 +0500

    Add account file

---
+++ Apteka.Backend/Repository/Account/IAdminAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Apteka.Backend.Model;
+
+namespace Apteka.Backend.Repository.Account
+{
+
+    public interface IAdminAccountService
+    {
+        Task<StateResponse<Admin>> SignUpAsync(Admin entity);
+        Task<StateResponse<Admin>> LogInAsync(string login, string password);
+        Task<StateResponse<bool>> LogOutAsync(string login, string password);
+        Task<StateResponse<IEnumerable<Admin>>> GetAllDataAsync();
+        Task<StateResponse<Admin>> GetByIdAsync(int id);
+    }
+}
