36ab08b677327dc9d62b2e64c9dd7c8a1a8cd221
USER-1 <USER-1@USER-1>
2023-11-07 20:24:30 +0500

    Added models

---
+++ Apteka.Backend/Migrations/20231107151913_AdminMigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Apteka.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    EmployeeRole = table.Column<int>(type: "integer", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "character varying(18)", maxLength: 18, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
---
+++ Apteka.Backend/Model/Payment.cs
@@ -0,0 +1,13 @@
+﻿using LinqToDB.Mapping;
+
+namespace Apteka.Backend.Model
+{
+    public class Payment
+    {
+        [Identity]
+        public Guid Id { get; set; }
+        public decimal AllMoney { get; set; }
+        public decimal Salary { get; set; }
+        public decimal AnotherExpense { get; set; }
+    }
+}
